992129083cd7084339e34bdc176e00bc6ff9ecba
Stekaka <oliver@dronarkompaniet.se>
2025-05-01 02:49:51 +0200

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
       'Authorization': `Token ${process.env.REPLICATE_API_TOKEN}`
     },
     body: JSON.stringify({
-      version: "7e8c1de0ceec49d7d53e2f7c81a7c1df4fd4f3b7b7b69f91caa8a5ff06e7e204", // Realistic Vision v5.1
+      version: "7e8c1de0ceec49d7d53e2f7c81a7c1df4fd4f3b7b7b69f91caa8a5ff06e7e204",
       input: {
         prompt,
         width: 512,
@@ -31,3 +31,4 @@ export default async function handler(req, res) {
   }
 
   res.status(200).json({ id: data.id });
+}
