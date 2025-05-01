a30660166bbacb71739b69b9abaa8ebb73ca33b7
Stekaka <oliver@dronarkompaniet.se>
2025-05-01 03:29:49 +0200

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
       'Authorization': `Token ${process.env.REPLICATE_API_TOKEN}`
     },
     body: JSON.stringify({
-      version: "7e8c1de0ceec49d7d53e2f7c81a7c1df4fd4f3b7b7b69f91caa8a5ff06e7e204",
+      version: "cc6e1b763e6c4e68be58b8695c3237f4213fbfd3c4570c3f61638e5cf59e442b", // stability-ai/sdxl
       input: {
         prompt,
         width: 512,
